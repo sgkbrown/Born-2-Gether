42ab64a74a618e625b75b1fd98757d6edcd687f6
Nutech Systems <nutechsystems@Michaels-MacBook-Air.local>
2014-10-28 17:40:53 -0400

    Initial Commit

---
+++ BornTogether/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BornTogether
+//
+//  Created by Nutech Systems on 10/28/14.
+//  Copyright (c) 2014 Stefan Brown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BornTogether/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BornTogether
+//
+//  Created by Nutech Systems on 10/28/14.
+//  Copyright (c) 2014 Stefan Brown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
